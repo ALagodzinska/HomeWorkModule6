2822ae74cf4818670babe57a955474dbbb6dd71f
ALagodzinska <anastasijalagodzinska@gmail.com>
2021-10-31 19:54:35 +0200

    Add project files.

---
+++ HomeWorkModule6/ArrayOperator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkModule6
+{
+    class ArrayOperator
+    {        
+        private int[] Array1;
+        private int[] Array2;
+        
+        public int[] array1 { get { return Array1; } }
+        public int[] array2 { get { return Array2; } }
+
+        public ArrayOperator(int[] array1, int[] array2)
+        {
+            Array1 = array1;
+            Array2 = array2;            
+        }
+
+        public void fillArray()
+        {
+            for (int i = 0; i < Array1.Length; i++)
+            {
+                Array2[i] = Array1[i];
+            }
+        }
+    }
+}
---
+++ HomeWorkModule6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HomeWorkModule6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array1 = { 12, 223, 3343, 45345, 23423, 23, 1112, 456, 33, 1 };
+            int[] Array2 = new int[10];
+
+            ArrayOperator arrayOperator = new ArrayOperator(Array1, Array2);
+
+            arrayOperator.fillArray();
+
+            Console.WriteLine($"Array1: {string.Join(", ", arrayOperator.array1)}\n" +
+                $"Populated Array2: {string.Join(", ", arrayOperator.array2)}");
+
+            Console.ReadLine();            
+        }
+    }
+}
